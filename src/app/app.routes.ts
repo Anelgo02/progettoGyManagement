e5867d795421e949f3efb7a8ec3c615bced43188
Angelo <1234retiu@gmail.com>
2025-05-16 18:06:27 +0200

    ho sistemato una cosa del routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,15 +13,13 @@ export const routes: Routes = [
   },
 
   {
-<<<<<<< HEAD
     path: 'prova',
     loadComponent: () => import('./components/header/header.component').then((m) => m.HeaderComponent),
   },
-=======
+  {
     path: 'Login',
     loadComponent: () => import('./components/login-page/login-page.component').then((m) => m.LoginPageComponent),
   }
->>>>>>> 01ef95b258fba1383f39c5f91c587d5ab9f7c66e
 
   
 ];
