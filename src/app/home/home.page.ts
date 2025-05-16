4533987b03732211340d8cdcd67037d62a13ff07
Angelo <1234retiu@gmail.com>
2025-05-16 18:27:29 +0200

    header sistemato

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonAvatar, IonIcon, IonButtons } from '@ionic/angular/standalone';
+import { HeaderComponent } from "../components/header/header.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent,],
+  imports: [IonButtons, IonIcon, IonAvatar, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, HeaderComponent],
 })
 export class HomePage {
   constructor() {}
