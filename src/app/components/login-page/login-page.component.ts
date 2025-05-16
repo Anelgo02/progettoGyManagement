10cea25b2038e8557fc849f798790be6f34ff2ff
Angelo <1234retiu@gmail.com>
2025-05-16 19:04:22 +0200

    login page sistemata con ionic

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,45 +1,53 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 import Swal from 'sweetalert2';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [
+    IonicModule,
+    FormsModule,
+    RouterModule,
+    HeaderComponent
+],
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-
 export class LoginPageComponent {
   email: string = '';
   password: string = '';
-  passwordVisible: boolean = false;
-
-  login() {
-    if (!this.email || !this.password) {
-      Swal.fire({
-        icon: 'error',
-        title: 'ERRORE',
-        text: 'Inserire le credenziali',
-      });
-    } else {
-      Swal.fire({
-        title: 'Accesso in corso...',
-        text: 'Attendi un attimo',
-        allowOutsideClick: false,
-        didOpen: () => {
-          Swal.showLoading();
-        },
-      });
-
-      setTimeout(() => {
-        Swal.fire({
-          title: 'Successo!',
-          text: 'Sei loggato',
-          icon: 'success',
-        });
-      }, 3000);
-    }
-  }
+  showPassword = false;
 
   togglePasswordVisibility() {
-    this.passwordVisible = !this.passwordVisible;
+    this.showPassword = !this.showPassword;
   }
+
+  login() {
+  if (!this.email || !this.password) {
+    setTimeout(() => {
+      Swal.fire({
+        title: 'Attenzione',
+        text: 'Email e password sono obbligatorie',
+        icon: 'warning',
+        heightAuto: false
+      });
+    }, 10);
+    return;
+  }
+
+  setTimeout(() => {
+    Swal.fire({
+      title: 'Login effettuato',
+      text: `Benvenuto, ${this.email}`,
+      icon: 'success',
+      heightAuto: false
+    });
+  }, 10);
+}
+
 }
