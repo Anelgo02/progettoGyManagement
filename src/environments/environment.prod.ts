783a08d042eb84e61ad440adeef99ed248489619
giuseppe5516 <giuseppe8537@gmail.com>
2025-05-31 18:05:46 +0200

    niente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiBase: 'http://192.168.1.101:5000'  
+  //apiBase: 'http://192.168.1.101:5000'  
+  apiBase: 'http://localhost:5000' 
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,9 @@
 
 export const environment = {
   production: false,
-  apiBase: 'http://192.168.1.101:5000'  
+  //apiBase: 'http://192.168.1.101:5000'  
+  apiBase: 'http://localhost:5000' 
 };
 
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
